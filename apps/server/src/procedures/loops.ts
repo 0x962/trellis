9887778b554110f755488e4f09afd9d56419430d
Navid Khan <n@nvdk.co>
2026-09-17 01:50:05 -0400

    Allow loop wait period updates

--- apps/server/src/procedures/loops.ts
+++ apps/server/src/procedures/loops.ts
@@ -3,4 +3,5 @@ import { call, os } from "./base.ts";
 export const loops = os.loops.router({
 	list: os.loops.list.handler(({ context, input }) => call(context, "loops.list", input)),
 	control: os.loops.control.handler(({ context, input }) => call(context, "loops.control", input)),
+	update: os.loops.update.handler(({ context, input }) => call(context, "loops.update", input)),
 });
