fceaa85084159050fd7bac80db1bb16dbc83149d
Navid Khan <n@nvdk.co>
2026-09-21 14:01:30 -0400

    Move epic statistics into the sheet stack

--- apps/web/src/stores/pageSheetStore/pageSheetStore.test.ts
+++ apps/web/src/stores/pageSheetStore/pageSheetStore.test.ts
@@ -3,23 +3,23 @@ import { pageSheetActions, usePageSheetStore } from "./pageSheetStore";
 
 const state = () => usePageSheetStore.getState();
 
-beforeEach(() => usePageSheetStore.setState({ ticket: null, pr: null }));
+beforeEach(() => usePageSheetStore.setState({ ticket: null, pr: null, stats: null }));
 
 test("the stack starts empty", () => {
-	expect(state()).toEqual({ ticket: null, pr: null });
+	expect(state()).toEqual({ ticket: null, pr: null, stats: null });
 });
 
 test("a pull request opens over the ticket and leaves it open", () => {
 	pageSheetActions.openTicket("TRL-42");
 	pageSheetActions.openPullRequest("https://github.com/o/r/pull/7");
 
-	expect(state()).toEqual({ ticket: "TRL-42", pr: "https://github.com/o/r/pull/7" });
+	expect(state()).toEqual({ ticket: "TRL-42", pr: "https://github.com/o/r/pull/7", stats: null });
 });
 
 test("a pull request opens with no ticket under it", () => {
 	pageSheetActions.openPullRequest("https://github.com/o/r/pull/7");
 
-	expect(state()).toEqual({ ticket: null, pr: "https://github.com/o/r/pull/7" });
+	expect(state()).toEqual({ ticket: null, pr: "https://github.com/o/r/pull/7", stats: null });
 });
 
 test("closing the pull request leaves the ticket open", () => {
@@ -27,7 +27,7 @@ test("closing the pull request leaves the ticket open", () => {
 	pageSheetActions.openPullRequest("https://github.com/o/r/pull/7");
 	pageSheetActions.closePullRequest();
 
-	expect(state()).toEqual({ ticket: "TRL-42", pr: null });
+	expect(state()).toEqual({ ticket: "TRL-42", pr: null, stats: null });
 });
 
 test("closing the ticket closes the pull request over it", () => {
@@ -35,7 +35,7 @@ test("closing the ticket closes the pull request over it", () => {
 	pageSheetActions.openPullRequest("https://github.com/o/r/pull/7");
 	pageSheetActions.closeTicket();
 
-	expect(state()).toEqual({ ticket: null, pr: null });
+	expect(state()).toEqual({ ticket: null, pr: null, stats: null });
 });
 
 test("another ticket closes the pull request of the ticket before it", () => {
@@ -43,5 +43,13 @@ test("another ticket closes the pull request of the ticket before it", () => {
 	pageSheetActions.openPullRequest("https://github.com/o/r/pull/7");
 	pageSheetActions.openTicket("TRL-43");
 
-	expect(state()).toEqual({ ticket: "TRL-43", pr: null });
+	expect(state()).toEqual({ ticket: "TRL-43", pr: null, stats: null });
+});
+
+test("statistics opens as the only sheet", () => {
+	pageSheetActions.openTicket("TRL-42");
+	pageSheetActions.openPullRequest("https://github.com/o/r/pull/7");
+	pageSheetActions.openStats("TRL/runtime");
+
+	expect(state()).toEqual({ ticket: null, pr: null, stats: "TRL/runtime" });
 });
